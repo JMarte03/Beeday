c6e3c63de5572db32ff249efa1c90ba6b82ae527
JMarte03 <collegeis2much@gmail.com>
2024-11-13 16:29:16 -0400

    login and signup routing

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express()
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('express-flash')
+const logger = require('morgan')
 
 require('dotenv').config({path: './config/.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -12,10 +21,30 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(logger('dev'))
+
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+    //   store: MongoStore.create({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({
+        client: mongoose.connection.getClient()
+      })
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
  
 app.listen(process.env.PORT, ()=>{
-    console.log('Server is running.')
+    console.log(`Server is running on ${process.env.PORT}`)
 })    
